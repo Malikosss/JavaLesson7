b2a509d192a62e58bca152703a2837c2c8a53e9b
Malikosss <malikosss@yandex.ru>
2022-09-13 16:15:19 +0300

    first commit

---
+++ src/test/java/ru/netology/stats/StatServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.stats.StatService;
+
+public class StatServiceTest {
+
+    @Test
+    public void test() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+
+}
